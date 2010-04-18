776e4873f55a911731a72847cca0077823afe127
Mike Dalessio <mike@csa.net>
2010-04-18 14:41:47 -0400

    refactored views and moving mcbeanification into the controller. support for textile.

--- mcbean-app.rb
+++ mcbean-app.rb
@@ -5,31 +5,20 @@ require 'sinatra'
 require 'open-uri'
 require 'mcbean'
 
-class Markdownify
-  def initialize(app, options={})
-    @app = app
-  end
-
-  def call(env)
-    result = @app.call(env)
-    if env["REQUEST_URI"] == "/fetch"
-      result[1] = {"Content-Type" => "text/plain"}
-      result[2] = McBean.document(result[2].first).to_markdown
-    end
-    result
-  end
-end
-
-use Markdownify
-
 get "/" do
   erb :index
 end
 
 post "/fetch" do
-  begin
-    open(params['url']).read
-  rescue
-    "Could not fetch the URL '#{params['url']}'"
+  @url = params['url']
+  @format = params['format']
+  unless McBean.formats.include?(@format)
+    @format = "markdown"
   end
+  @content = begin
+               McBean.document(open(@url).read).send("to_#{@format}")
+             rescue
+               $!.to_s
+             end
+  erb :result
 end
